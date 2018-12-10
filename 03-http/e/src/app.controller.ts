3c98476412797a2333b802c844df011a20d43ed8
AlexaVane <alexavanemacas321@gmail.com>
2018-12-10 14:38:10 -0500

    deber

--- 03-http/e/src/app.controller.ts
+++ 03-http/e/src/app.controller.ts
@@ -1,12 +1,157 @@
-import { Get, Controller } from '@nestjs/common';
+import {
+    Get,
+    Controller,
+    Post,
+    HttpCode,
+    UnauthorizedException,
+    Body,
+    Req,
+    Res,
+    Query,
+    Put,
+    Delete
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import {Request, Response} from 'express';
+import {Profiler} from "inspector";
+//import PositionTickInfo = module
+import $JestEnvironment = jest.$JestEnvironment;
+import {Observable, of} from "rxjs";
+
 
 @Controller()
 export class AppController {
+    [x: string]: any;
   constructor(private readonly appService: AppService) {}
 
   @Get()
   root(): string {
-    return this.appService.root();
+    return 'hii';
   }
+
+  @Post()
+  rooxt():string{
+    return 'd'
+  }
+    @Post('crearMedicina')
+    @HttpCode(200)  // Codigo OK
+    crearMedicina(
+        @Res() res: Response,
+        @Req() req: Request | any,
+        @Body() medicina: Medicina,
+        @Body('nombre') nombre1: string,
+        @Body(' presentacion') presentacion: string,
+        @Body('precio') precio: number,
+        @Body('fecha') fecha: Date,
+
+
+    ) {
+
+        const bdd = this._appService.crearMedicina(medicina);
+        console.log(medicina);
+        res.json(bdd);
+
+    }
+
+    ////crearTipo
+    @Post('crearTipo')
+    @HttpCode(200)  // Codigo OK
+    crearTipo(
+        @Res() res: Response,
+        @Req() req: Request | any,
+        @Body() tipomedicina: TipoMedicina,
+        @Body('nombre') nombre: string,
+        @Body(' uso') uso: string,
+        @Body('cantidad') cantidad: number,
+        @Body('fecha') fecha: Date,
+
+
+    ) {
+
+        const bdd = this._appService.crearTipo(tipomedicina);
+        console.log(tipomedicina);
+        res.json(bdd);
+
+    }
+    ///// buscar medicina
+    @Get('buscarMedicina')
+
+    @HttpCode(204) // status
+    buscarMedicina(
+
+        @Query('nombre') nombre1: Medicina,
+    ) {
+
+        return  nombre1.nombre1;
+    }
+    ///buscar tipodemedicina
+    @Get('buscarTipoMedicina')
+
+    @HttpCode(204) // status
+    buscarTipoMedicina(
+
+        @Query('nombre') nombre: TipoMedicina, // adrian
+    ) {
+
+        return  nombre.nombre ;
+    }
+    ///actualizar
+    @Put('actualizarMedicina')
+
+    @HttpCode(204) // status
+    actualizarMedicina(
+
+        @Query('nombre') nombre1: Medicina,
+    ) {
+
+        return  nombre1.nombre1;
+    }
+
+///wliminar
+    @Delete('eliminarMedicina')
+
+    @HttpCode(204) // status
+    eliminarMedicina(
+
+        @Query('nombre') nombre1: Medicina,
+
+    ) {
+        const  respuestaBDD = RespuestaBDD.indiceMedicina;
+        const a = respuestaBDD.bdd.medicina
+        a.splice(respuestaBDD.bdd.medicina[nombre1],1)
+
+        //    medicina.splice(respuestaBDD.bdd.usuarios[indice3],1);
+
+
+        // indice3.splice(respuestaBDD.bdd.usuarios[indice],1)
+        return respuestaBDD;
+    }
+
+
+
+
+
+
+
+
 }
+
+
+
+
+export interface Medicina {
+    nombre1: string;
+    presentacion: string;
+    precio: number;
+    fecha: Date;
+
+}
+
+export interface TipoMedicina {
+    nombre: string;
+    uso: string;
+    cantidad: number;
+    fecha: Date;
+
+}
+
--- 03-http/e/src/app.service.ts
+++ 03-http/e/src/app.service.ts
@@ -1,8 +1,228 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import {Medicina, TipoMedicina} from './app.controller';
+import * as rxjs from "rxjs";
+//import {mergeMap} from 'rxjs/operators'.mergeMap;
+//import * as map from 'rxjs/operators';
+const mergeMap = require('rxjs/operators').mergeMap;
+const map = require('rxjs/operators').map;
 
 @Injectable()
 export class AppService {
-  root(): string {
-    return 'Hello World!';
-  }
+    bdd: Medicina[] = [];   // ARCHIVO .JSON
+
+  //  crearMedicina(medicina: Medicina) {
+    //    this.bdd.push(medicina);
+      //  return this.bdd;
+ //   }
+
+    crearMedicina(medicina: Medicina) {
+            inicialiarBDD();
+            const  respuestaBDD : RespuestaBDD;
+             medicina = respuestaBDD.Medicina;
+             respuestaBDD.bdd.medicina.push(medicina);
+
+        }
+        crearTipo(tipomedicina: TipoMedicina) {
+            inicialiarBDD();
+            const  respuestaBDD : RespuestaBDD;
+             tipomedicina = respuestaBDD.TipoMedicina;
+             respuestaBDD.bdd.tipomedicina.push(tipomedicina);
+
+        }
+
+    buscarMedicina(respuestaBDD: RespuestaBDD){
+        const indice = respuestaBDD.bdd
+            .medicina
+            .findIndex( // -1
+                (medicina: any) => {
+
+                    return medicina.indiceMedicina === indice;
+
+                }
+            );
+
+        respuestaBDD = indice;
+
+        return respuestaBDD;
+
+        }
+
+    buscarTipoMedicina(respuestaBDD: RespuestaBDD){
+        const indice = respuestaBDD.bdd.Tipomedicina
+                      .findIndex( // -1
+                (tipomedicina: any) => {
+
+                    return tipomedicina.indiceTipoMedicina === indice;
+
+                }
+            );
+
+        respuestaBDD = indice;
+
+        return respuestaBDD;
+
+    }
+
+    actualizarMedicina(respuestaBDD: RespuestaBDD){
+        const indice = respuestaBDD.bdd
+            .medicina
+            .findIndex( // -1
+                (medicina: any) => {
+
+                   if  (medicina.indiceMedicina === indice){
+                    map(
+                        (nombre:{nombre1:string})=>{
+                            respuestaBDD.medicina ={
+                                nombre:nombre
+                            };
+                            return respuestaBDD;
+                        }
+
+                }
+                );
+    }
+
+
+        return respuestaBDD;
+
+    }
+
+    ////eliminar
+
+eliminarMedicina(respuestaBDD: RespuestaBDD){
+    const indice = respuestaBDD.bdd
+        .medicina
+        .findIndex( // -1
+            (medicina: any) => {
+
+                if  (medicina.indiceMedicina === indice){
+                    map(
+                        (nombre:{nombre1:string})=>{
+                            respuestaBDD.medicina ={
+                                nombre:nombre
+                            };
+                            return respuestaBDD;
+                        }
+
+                }
+            );
 }
+}
+
+
+
+}
+function inicialiarBDD() {
+
+    return new Promise(
+        (resolve, reject) => {
+            fs.readFile(
+                'bdd.json',
+                'utf-8',
+                (error, contenidoArchivo) => { // CALLBACK
+                    if (error) {
+
+                        fs.writeFile(
+                            'bdd.json',
+                            '{"medicina":[],"tipodemedicina":[]}',
+                            (error) => {
+                                if (error) {
+                                    reject({
+                                        mensaje: 'Error creando',
+                                        error: 500
+                                    })
+                                } else {
+                                    resolve({
+                                        mensaje: 'BDD leida',
+                                        bdd: JSON.parse('{"medicina":[],"tipodemedicina":[]}')
+                                    })
+                                }
+
+                            }
+                        )
+
+                    } else {
+                        resolve({
+                            mensaje: 'BDD leida',
+                            bdd: JSON.parse(contenidoArchivo)
+                        })
+                    }
+                }
+            )
+        }
+    );
+
+}
+
+
+function guardarBDD(bdd: BDD) {
+    return new Promise(
+        (resolve, reject) => {
+            fs.writeFile(
+                'bdd.json',
+                JSON.stringify(bdd),
+                (error) => {
+                    if (error) {
+                        reject({
+                            mensaje: 'Error creando',
+                            error: 500
+                        })
+                    } else {
+                        resolve({
+                            mensaje: 'BDD guardada',
+                            bdd: bdd
+                        })
+                    }
+
+                }
+            )
+        }
+    )
+}
+function guardarBaseDeDatos() {
+    return mergeMap(// Respuesta del anterior OBS
+        (respuestaBDD: RespuestaBDD) => {
+            // OBS
+            return rxjs.from(guardarBDD(respuestaBDD.bdd))
+        }
+    )
+}
+
+
+
+    // of(Cualquier cosa JS)
+    // from(Promesas)
+
+ //   const respuestaBDD$ = rxjs.from(inicialiarBDD());
+
+   // respuestaBDD$
+     //   .pipe(
+
+
+       //     guardarBaseDeDatos()
+        //)
+
+
+
+
+
+
+
+interface RespuestaBDD {
+    mensaje: string;
+    bdd: BDD;
+    medicina?: Medicina;
+    indiceMedicina?: number;
+    tipomedicina?: TipoMedicina;
+    indiceTipoMedicina?: number;
+
+}
+
+interface BDD {
+    medicina: Medicina[] | any;
+    Tipomedicina: TipoMedicina[] | any;
+
+}
+
+
